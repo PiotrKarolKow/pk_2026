b210f8aefc19cd619302cdbee61cb5635a83f07b
PiotrKarolKow <piotr.kowalczyk1210@gmail.com>
2026-01-03 10:07:47 +0000

    Initiatl class Czlowiek

---
+++ klasy.py
@@ -0,0 +1,24 @@
+
+# Klasa = szablon, przepis
+# Istota
+# Zamysl
+class Czlowiek:
+    #Istota
+    gatunek = "Homo Sapiens"
+    def __init__(self):
+        #Akt istnienia
+        print("Niech powstanie Czlowiek")
+
+# Powstanie obiektu
+# Gotowanie z przepisu
+
+adam = Czlowiek()
+print(adam.gatunek)
+#print(type(adam))
+#print(dir(adam))
+
+
+#print(type(Czlowiek))
+#print(dir(Czlowiek))
+
+
