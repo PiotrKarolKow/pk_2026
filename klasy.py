d0583f64d559ca6ce4eb9244928c5786ec4cbcf1
PiotrKarolKow <piotr.kowalczyk1210@gmail.com>
2026-01-03 10:29:05 +0000

    imie w klasie czlowiek

--- klasy.py
+++ klasy.py
@@ -5,15 +5,21 @@
 class Czlowiek:
     #Istota
     gatunek = "Homo Sapiens"
-    def __init__(self):
+    def __init__(self, imie):
         #Akt istnienia
-        print("Niech powstanie Czlowiek")
+        print(f"Niech powstanie Czlowiek o imieniu {imie} ")
+        self.imie = imie
 
-# Powstanie obiektu
+# Powstanie obiektuu
 # Gotowanie z przepisu test
 
-adam = Czlowiek()
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
 print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
+
 #print(type(adam))
 #print(dir(adam))
 
